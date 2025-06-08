97b545f4269a25e332366ec4ff19a49d15009d6c
Afaq_Ahmad_eng <afaqahmad2593@gmail.com>
2025-06-08 21:37:14 +0500

    Update Header.js file

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import Logo from '../assests/images/logo.jpg'
+import style from './Header.module.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 function Header() {
   return (
     <>
-    
+    <BrowserRouter>
+    <header>
+        <meta name="description" content="your text goes here"/>
+        <meta name="og:title" content="little lemon restaurant"/>
+        <meta name="og:description" content="We provide best food to you"/>
+    </header>
+    <nav>
+    <img src={Logo} alt='logo' id={style.logoImage}/>
+    <ul>
+        <Link></Link>
+    </ul>
+    </nav>
+    <Routes>
+        <Route/>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
