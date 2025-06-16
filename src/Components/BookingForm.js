c7c01166dd048d56676b6e517730715acef2db08
Afaq_Ahmad_eng <afaqahmad2593@gmail.com>
2025-06-16 22:53:31 +0500

    Create and then complete the BookingForm component.

---
+++ src/Components/BookingForm.js
@@ -0,0 +1,75 @@
+import styles from './BookingForm.module.css';
+
+const BookingForm = ({
+  date,
+  setDate,
+  time,
+  setTime,
+  guests,
+  setGuests,
+  occasion,
+  setOccasion,
+  availableTimes,
+  onSubmit,
+}) => {
+  return (
+    <form onSubmit={onSubmit} className={styles.form}>
+      <h2 className={styles.heading}>Make a Reservation</h2>
+
+      <label htmlFor="res-date">Choose date</label>
+      <input
+        type="date"
+        id="res-date"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+        required
+        className={styles.input}
+      />
+
+      <label htmlFor="res-time">Choose time</label>
+      <select
+        id="res-time"
+        value={time}
+        onChange={(e) => setTime(e.target.value)}
+        className={styles.input}
+      >
+        {availableTimes.map((t) => (
+          <option key={t} value={t}>
+            {t}
+          </option>
+        ))}
+      </select>
+
+      <label htmlFor="guests">Number of guests</label>
+      <input
+        type="number"
+        id="guests"
+        min="1"
+        max="10"
+        value={guests}
+        onChange={(e) => setGuests(parseInt(e.target.value))}
+        required
+        className={styles.input}
+      />
+
+      <label htmlFor="occasion">Occasion</label>
+      <select
+        id="occasion"
+        value={occasion}
+        onChange={(e) => setOccasion(e.target.value)}
+        className={styles.input}
+      >
+        <option value="Birthday">Birthday</option>
+        <option value="Anniversary">Anniversary</option>
+      </select>
+
+      <input
+        type="submit"
+        value="Make Your Reservation"
+        className={styles.button}
+      />
+    </form>
+  );
+};
+
+export default BookingForm;
