7de6fc09316a349040bda5cc51d201ed834f099b
Afaq_Ahmad_eng <afaqahmad2593@gmail.com>
2025-06-19 22:49:16 +0500

    update the Navbar according to requirement

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,6 @@
 import React, { useState } from 'react';
 import Logo from '../assests/images/logo.jpg';
 import style from './Navbar.module.css';
-import About from './About';
-import Contact from './Contact';
-import Reservation from './Reservation';
-import Chicago from './Chicago';
-import CustomersSay from './CustomersSay';
-import CallToAction from './ConfirmedBooking';
-import Specials from './Specials';
-import { Routes, Route, Link } from 'react-router-dom';
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -17,9 +9,9 @@ function Navbar() {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  const closeMenu = () => {
-    setIsMenuOpen(false);
-  };
+  // const closeMenu = () => {
+  //   setIsMenuOpen(false);
+  // };
 
   return (
     <>
@@ -38,31 +30,12 @@ function Navbar() {
           </button>
           
           <ul className={`${style.navigation} ${isMenuOpen ? style.active : ''}`}>
-            <li><Link className={style.links} to='/about' onClick={closeMenu}>About</Link></li>
-            <li><Link className={style.links} to='/contact' onClick={closeMenu}>Contact</Link></li>
-            <li><Link className={style.links} to='/Reservation' onClick={closeMenu}>Reservation</Link></li>
-            <li><Link className={style.links} to='/Chicago' onClick={closeMenu}>Chicago</Link></li>
-            <li><Link className={style.links} to='/CustomersSay' onClick={closeMenu}>Customer Say</Link></li>
-            <li><Link className={style.links} to='/CallToAction' onClick={closeMenu}>Call To Action</Link></li>
-            <li><Link className={style.links} to='/Specials' onClick={closeMenu}>Specials</Link></li>
-            
             <div className={style.authButtons}>
               <button className={style.SignUpAndSigninButtons}>Sign Up</button>
               <button className={style.SignUpAndSigninButtons}>Sign In</button>
             </div>
           </ul>
         </nav>
-        
-        <Routes>
-          <Route path='/About' element={<About/>}/>
-          <Route path='/Contact' element={<Contact/>}/>
-          <Route path='/Reservation' element={<Reservation/>}/>
-          <Route path='/Chicago' element={<Chicago/>}/>
-          <Route path='/CustomersSay' element={<CustomersSay/>}/>
-          <Route path='/CallToAction' element={<CallToAction/>}/>
-          <Route path='/CallToAction' element={<CallToAction/>}/>
-          <Route path='/Specials' element={<Specials/>}/>
-        </Routes>
     </>
   )
 }
